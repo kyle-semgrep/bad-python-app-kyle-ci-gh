ad24ba187d707adab069ec8c39c7eb1d0edb9161
hyjinx187 <30838651+hyjinx187@users.noreply.github.com>
2025-03-16 17:48:21 -0400

    Update vuln-25.java
    Co-authored-by: semgrep-code-kyle[bot] <202702614+semgrep-code-kyle[bot]@users.noreply.github.com>

--- vuln-25.java
+++ vuln-25.java
@@ -12,7 +12,57 @@ public class App
     private static void bad1() {
         try {
             // ruleid: java-jwt-hardcoded-secret
-            Algorithm algorithm = Algorithm.HMAC256("secret");
+            package jwt_test.jwt_test_1;
+
+            import com.auth0.jwt.JWT;
+            import com.auth0.jwt.algorithms.Algorithm;
+            import com.auth0.jwt.exceptions.JWTCreationException;
+
+            public class App
+            {
+
+                static String secret = "secret";
+
+                private static void bad1(String secretKey) {
+                    try {
+                        // fix: java-jwt-hardcoded-secret
+                        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+                        String token = JWT.create()
+                            .withIssuer("auth0")
+                            .sign(algorithm);
+                    } catch (JWTCreationException exception){
+                        //Invalid Signing configuration / Couldn't convert Claims.
+                    }
+                }
+
+                private static void ok1(String secretKey) {
+                    try {
+                        // ok: java-jwt-hardcoded-secret
+                        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+                        String token = JWT.create()
+                            .withIssuer("auth0")
+                            .sign(algorithm);
+                    } catch (JWTCreationException exception){
+                        //Invalid Signing configuration / Couldn't convert Claims.
+                    }
+                }
+
+                public static void main( String[] args )
+                {
+                    // Retrieve the secret key from an environment variable
+                    String secretKey = System.getenv("SECRET_KEY");
+                    if (secretKey == null) {
+                        throw new IllegalArgumentException("SECRET_KEY environment variable not set");
+                    }
+
+                    bad1(secretKey);
+                    ok1(args[0]);
+                }
+            }
+
+            abstract class App2
+            {
+            // ruleid: java-jwt-hardcoded-secret
             String token = JWT.create()
                 .withIssuer("auth0")
                 .sign(algorithm);
